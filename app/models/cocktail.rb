9956b28c12cb254947da7fa9f26935c328e980b3
David Smith <smitty6000@gmail.com>
2021-01-21 16:37:02 +0900

    fixed validations

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,6 @@
 class Cocktail < ApplicationRecord
     validates :name, uniqueness: true, presence: true
+    validates :photo, presence: true
     has_many :doses, dependent: :destroy
     has_many :ingredients, through: :doses
     has_one_attached :photo
